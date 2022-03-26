2bbd5a4dde933815aeab28a3f4a0bc828e8208e8
RyanGitHub101 <96314042+RyanGitHub101@users.noreply.github.com>
2022-03-26 16:24:21 +0000

    Delete My programs/Percentage-calculator- directory

--- My programs/Percentage-calculator-/percentage.js
+++
@@ -1,9 +0,0 @@
-function percentIncrease(originalNum, newNum) {
-    let substractAnswer = newNum - originalNum;
-    let fractionAnswer = substractAnswer / originalNum
-    let multiply100 = fractionAnswer * 100;
-    return multiply100 + "%";
-}
- console.log(percentIncrease());
-
- /*MADE BY RyanGitHub101*/
