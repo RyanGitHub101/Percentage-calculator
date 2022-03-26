419214ccd9d3bc10a496e4c2a5eedc7be02a4410
RyanGitHub101 <96314042+RyanGitHub101@users.noreply.github.com>
2022-03-26 16:22:11 +0000

    Add files via upload
    Folders

---
+++ My programs/Percentage-calculator-/percentage.js
@@ -0,0 +1,9 @@
+function percentIncrease(originalNum, newNum) {
+    let substractAnswer = newNum - originalNum;
+    let fractionAnswer = substractAnswer / originalNum
+    let multiply100 = fractionAnswer * 100;
+    return multiply100 + "%";
+}
+ console.log(percentIncrease());
+
+ /*MADE BY RyanGitHub101*/
