5715c0e54c074f7844f9a3c9255240eeb021762f
RyanGitHub101 <96314042+RyanGitHub101@users.noreply.github.com>
2022-04-03 16:56:56 +0100

    Add files via upload

---
+++ salary_tax.js
@@ -0,0 +1,9 @@
+
+function salary(salary_number, income_tax) {
+     let divide_both = salary_number / income_tax;
+     let multiply_by_100 = divide_both * 100;
+     let both100 =  10000 / multiply_by_100;
+     return both100 + "%"
+};
+console.log(salary(20000, 5000));
+/* MADE BY RyanGitHub101 */
